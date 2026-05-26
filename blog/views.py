eda26c6d030bdf575e29b0585b263ae00f45c979
Hugo <gangneuxhugo1@gmail.com>
2026-05-26 16:20:17 +0200

    Ajout de l'authentification login/mdp, connection/deconnexion

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .forms import PostForm
@@ -13,6 +14,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required 
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -25,7 +27,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
+@login_required 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
